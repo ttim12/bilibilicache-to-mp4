9880678ea591c5dcf39f89238f9852f38037ee12
test <test@example.com>
2026-09-11 23:29:21 +0800

    优化使用体验

--- bilibilimp4.py
+++ bilibilimp4.py
@@ -1,11 +1,24 @@
-import os,subprocess,shutil
+import os,json,time,subprocess,shutil
+import tkinter as tk
+from tkinter import filedialog
 
-path=input('请输入缓存视频文件夹路径')
+root = tk.Tk()
+root.withdraw()
+root.wm_attributes("-topmost",1)
+path = filedialog.askdirectory(title='请选择缓存视频所在的文件夹')
+if not path:
+    print('未选择路径')
+    time.sleep(1)
+    exit()
+#path=input('请输入缓存视频文件夹路径')
+
 
 print(f'目录文件:{os.listdir(path)}\n开始查找视音频\n')
 m4s = [f for f in os.listdir(path) if f.endswith('.m4s')]
 audio = [i for i in m4s if '30280' in i]
 video = [i for i in m4s if '30280' not in i]
+title = [f for f in os.listdir(path) if f.endswith('.json')]
+
 
 #用临时文件操作
 audio_temp = os.path.join(path, 'audio_temp.m4s')
@@ -13,8 +26,8 @@ video_temp = os.path.join(path, 'video_temp.m4s')
 shutil.copy(os.path.join(path,audio[0]), audio_temp)
 shutil.copy(os.path.join(path,video[0]), video_temp)
 
-#audio_temp和video_temp已经是路径.
 
+#audio_temp和video_temp已经是路径.
 for i in(audio_temp,video_temp):          #删开头9个0
     with open(i,'rb') as f:
         data = f.read()
@@ -27,15 +40,24 @@ for i in(audio_temp,video_temp):          #删开头9个0
     with open(i,'wb') as f:
         f.write(data)
 
-#检测使用系统的还是工具的ffmpeg
 
+with open(os.path.join(path,title[0]),'r',encoding='utf-8') as f:
+    data = json.load(f)
+    tabname = data['tabName']
+    Title = tabname[:8]
+    if len(Title)==8:
+        Title += 'xxx'
+
+
+#检测使用系统的还是工具的ffmpeg
 try:
     import imageio_ffmpeg
     ffmpeg = imageio_ffmpeg.get_ffmpeg_exe()
 except:
     ffmpeg = shutil.which('ffmpeg')
 
-subprocess.run([ffmpeg,'-v','error','-i',video_temp,'-i',audio_temp,'-c','copy',os.path.join(path,'output.mp4')])
+subprocess.run([ffmpeg,'-v','error','-i',video_temp,'-i',audio_temp,'-c','copy',os.path.join(path,f'{Title}.mp4')])
+
 
 os.remove(video_temp);os.remove(audio_temp)
 print('结束')
